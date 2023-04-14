50ad4dc65e9fb8f1f75c51fab0f8304c22f5086d
DiogoLeonardoo <diogoleo57@gmail.com>
2023-04-14 14:47:01 -0300

    atualização

--- testeJs.js
+++ testeJs.js
@@ -1,4 +1,20 @@
-var nome = 'Diogo'
-let idade = 19 
+const precoEt = 3.5
+const precoGasolina = 4.5
+const kmPorLitros = 10 
+const distanciaEmKm = 300
+const tipoCombustivel = 'Gasolina'
 
-console.log(nome + idade)
+const litrosConsumidos = (distanciaEmKm/kmPorLitros);
+
+if (tipoCombustivel === 'Etanol') {
+    const GastoTotal = litrosConsumidos * precoEt
+    console.log(GastoTotal.toFixed(2));
+}
+
+else {
+    const GastolTotal = litrosConsumidos * precoGasolina
+    console.log(GastolTotal.toFixed(2));
+}
+
+
+
